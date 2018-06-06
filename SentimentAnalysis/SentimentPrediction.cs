fff6c294846a6f15fe9c989c9cb5ebeed17bc8e4
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-06-06 14:28:18 +1000

    Test data and model definition classes.

---
+++ SentimentAnalysis/SentimentPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace SentimentAnalysis
+{
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Sentiment;
+    }
+}
